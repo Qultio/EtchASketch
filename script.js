b70190347aa47df0d9f1d338e902f8189aeaf521
Benjamin <Benjaminneyt@gmail.com>
2023-09-30 16:31:54 +0200

    Trying stuff, Don't know what I'm doing

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello world ;) ")
+/* let container = document.getElementById('container');
+for (let step = 0; step < 256; step++){
+    const div = document.createElement("div");
+    div.className = "grid";
+    container.appendChild(div)
+} */
+
+const container = document.getElementById("container");
+
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    container.appendChild(cell).className = "grid-item";
+  };
+};
+
+makeRows(16, 16);
